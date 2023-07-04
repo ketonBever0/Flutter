b9db1c05eca0bf76a7dbc46f68f6efbb9996d29d
Kurucz Laci <lacikurucz09@gmail.com>
2023-07-04 12:30:24 +0200

    ninja_id

---
+++ ninja_id/android/app/src/main/kotlin/laci/hu/ninja_id/MainActivity.kt
@@ -0,0 +1,6 @@
+package laci.hu.ninja_id
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ninja_id/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ninja ID Card'),
+      ),
+    );
+  }
+}
+
