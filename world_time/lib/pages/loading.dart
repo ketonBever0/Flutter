3ccd03f73b19c64a49377f7e6ea4880dd0f787e8
Kurucz Laci <lacikurucz09@gmail.com>
2023-08-06 17:32:33 +0200

    world time kis haladás

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -10,31 +11,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    Response response = await get(
-        Uri.parse('https://worldtimeapi.org/api/timezone/Europe/Budapest'));
-    Map data = jsonDecode(response.body);
+  
+  String time = 'loading';
 
-    String dateTime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    print(offset);
-
-    DateTime now = DateTime.parse(dateTime);
-
-    now = now.add(Duration(hours: int.parse(offset)));
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: "Berlin", flag: "germany.png", url: 'Europe/Berlin');
+    await instance.getTime();
+    setState(() {
+      time = instance.time;
+    });
 
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('loading screen'),
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(50),
+        child: Text(time)
+      )
     );
   }
 }
